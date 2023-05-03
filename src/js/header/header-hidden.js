3b165facb7e7de1bb38be406397ea2ef89d4fdd4
Serhii Revva <besha2vox@gmail.com>
2023-05-03 21:30:51 +0300

    fix: close dropdown when header is hidden

--- src/js/header/header-hidden.js
+++ src/js/header/header-hidden.js
@@ -1,3 +1,5 @@
+import { dropdownList, closeDropdownMenu } from './dropdown-btn';
+
 let prevScrollPos = window.pageYOffset;
 
 window.onscroll = function () {
@@ -7,6 +9,10 @@ window.onscroll = function () {
     document.querySelector('.header').classList.remove('header--hidden');
   } else {
     document.querySelector('.header').classList.add('header--hidden');
+
+    if (dropdownList.classList.contains('header__dropdown-list--open')) {
+      closeDropdownMenu();
+    }
   }
 
   prevScrollPos = currentScrollPos;
