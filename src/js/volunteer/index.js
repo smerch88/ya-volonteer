1fc22e3f7b8dc55ce41e2a982718ac2217d43e87
Serhii Revva <besha2vox@gmail.com>
2023-05-10 20:43:39 +0300

    complete sidebar

--- src/js/volunteer/index.js
+++ src/js/volunteer/index.js
@@ -2,6 +2,7 @@ import '../header/menu';
 import '../header/header-hidden';
 import '../header/dropdown-btn';
 import './volunteer';
+import './volunteer-burger';
 import './sticky-sidebar';
 
 const { height: pageHeaderHeight } = document
