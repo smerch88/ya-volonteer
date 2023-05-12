0aa92d14b9f85e2f6658fbd6d6e9be78d1c17e50
Serhii Revva <besha2vox@gmail.com>
2023-05-12 15:36:56 +0300

    feat: add section register-steps

--- src/js/volunteer/index.js
+++ src/js/volunteer/index.js
@@ -3,6 +3,7 @@ import '../header/header-hidden';
 import '../header/dropdown-btn';
 import './volunteer';
 import './volunteer-burger';
+import './button-to-bank';
 import './sticky-sidebar';
 import '../bank/bank';
 
